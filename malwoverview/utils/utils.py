65bb8ac3c46f570a63629b253e92b7db8533cd04
Ali Emara <aliemara175@gmail.com>
2024-10-01 22:28:37 +0300

    Update utils.py

--- malwoverview/utils/utils.py
+++ malwoverview/utils/utils.py
@@ -2,27 +2,30 @@ from urllib.parse import urlparse
 import geocoder
 import socket
 
+def urltoip(url_target):
+    """
+    Resolves a URL to its IP address and attempts to geolocate it.
 
-def urltoip(urltarget):
-    geoloc = ''
-    target = ''
-    finalip = ''
-    result = ''
+    Args:
+        url_target (str): The URL to resolve.
 
+    Returns:
+        str: The city of the geolocated IP address, or "Not Found" if the IP address cannot be resolved or geolocated.
+    """
     try:
-        target = urlparse(urltarget)
+        # Parse the URL to extract the network location
+        target = urlparse(url_target)
         result = target.netloc
-        finalip = socket.gethostbyname(result)
-        if finalip is not None:
-            geoloc = geocoder.ip(finalip)
-            if (geoloc is not None):
-                return geoloc.city
-            else:
-                result = ''
-                return result
-        else:
-            result = "Not Found"
-            return result
-    except Exception:
-        result = "Not Found"
-        return result
+
+        # Resolve the network location to an IP address
+        final_ip = socket.gethostbyname(result)
+
+        # Attempt to geolocate the IP address
+        geoloc = geocoder.ip(final_ip)
+
+        # Return the city of the geolocated IP address, or "Not Found" if it cannot be geolocated
+        return geoloc.city if geoloc else "Not Found"
+    except Exception as e:
+        # Log the exception and return "Not Found"
+        print(f"An error occurred: {e}")
+        return "Not Found"
